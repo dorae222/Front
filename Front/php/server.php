7678d52891e16ace7634cb4f21fd5798e003427e
도형준 <105966480+dorae222@users.noreply.github.com>
2022-12-02 17:09:55 +0900

    update

---
+++ Front/php/server.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+    <p>여기는 server.php 페이지로 이동한 것입니다.</p>
+
+    전달받은 히든은 <?php echo $_POST['hidden']; ?> 입니다.<br>
+    전달받은 아이디는 <?php echo $_POST['text']; ?> 입니다.<br>
+    전달받은 비밀번호는 <?php echo $_POST['password']; ?> 입니다.<br>
+    전달받은 텍스트는 <?php echo $_POST['text_2']; ?> 입니다.<br>
+    전달받은 검색창은 <?php echo $_POST['search']; ?> 입니다.<br>
+    전달받은 URL주소는 <?php echo $_POST['url']; ?> 입니다.<br>
+    전달받은 이메일은 <?php echo $_POST['email']; ?> 입니다.<br>
+    전달받은 전화번호는 <?php echo $_POST['tel']; ?> 입니다.<br>
+    전달받은 숫자는 <?php echo $_POST['email']; ?> 입니다.<br>
+    전달받은 범위는 <?php echo $_POST['range']; ?> 입니다.
+    </body>
+</html>
